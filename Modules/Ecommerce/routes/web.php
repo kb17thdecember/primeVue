dcbf8d95e4b38d4e68abba0393dcf5555a404b50
kbs <nguyentuankietdz@gmail.com>
2025-06-05 13:14:02 +0700

    delete modules

--- Modules/Ecommerce/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Ecommerce\Http\Controllers\EcommerceController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('ecommerces', EcommerceController::class)->names('ecommerce');
-});
